78f945422fbef9cac294dbb51e924229e6bb0f19
Ayomide Philip <a.areo.csc2023064@student.oauife.edu.ng>
2025-11-26 11:23:24 +0100

    Add authentication check and redirect to overview for logged-in users

--- app/(root)/(authentication)/layout.js
+++ app/(root)/(authentication)/layout.js
@@ -1,5 +1,10 @@
+import { auth } from "@/auth";
+import { redirect } from "next/navigation";
+
 /* eslint-disable @next/next/no-img-element */
 export default async function Layout({ children }) {
+  const session = await auth();
+  if (session && session.user) return redirect("/overview");
   return (
     <div className="min-h-screen flex items-center justify-center px-6 py-12 mt-10 bg-gray-50 dark:bg-gray-900">
       <div className="w-full max-w-6xl grid grid-cols-1 md:grid-cols-2 bg-white dark:bg-gray-800 shadow-2xl rounded-3xl overflow-hidden">
