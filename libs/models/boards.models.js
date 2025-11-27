89130f633939879d9bb0a147ed4c43931e1c27e6
Ayomide Philip <a.areo.csc2023064@student.oauife.edu.ng>
2025-11-27 04:20:16 +0100

    fix: Improve formatting and structure of boards schema in models

--- libs/models/boards.models.js
+++ libs/models/boards.models.js
@@ -1,2 +1,89 @@
-import {model, models, Schema} from "mongoose";
+import { model, models, Schema } from "mongoose";
 
+const boardsSchema = new Schema(
+  {
+    userId: {
+      type: Schema.Types.ObjectId,
+      ref: "Users",
+      required: true,
+      unique: true,
+    },
+    name: {
+      type: String,
+      required: true,
+      minlength: 5,
+      maxlength: 50,
+    },
+    tagline: {
+      type: String,
+      minlength: 5,
+    },
+    description: {
+      type: String,
+      required: true,
+      minlength: 10,
+    },
+    boardType: {
+      type: String,
+      enum: ["school", "projects", "group", "personal"],
+      default: "school",
+    },
+    school: {
+      type: {
+        name: {
+          type: String,
+        },
+        logo: {
+          type: String,
+        },
+        shortName: {
+          type: String,
+        },
+        country: {
+          type: String,
+        },
+      },
+    },
+    joinMode: {
+      type: String,
+      enum: ["code", "limited"],
+      default: "code",
+    },
+    seatLimit: {
+      type: Number,
+    },
+    allowComments: {
+      type: Boolean,
+      default: true,
+    },
+    allowPost: {
+      type: Boolean,
+      default: false,
+    },
+    course: {
+      type: [Schema.Types.ObjectId],
+      ref: "Courses",
+      default: [],
+    },
+    roles: {
+      type: {
+        admin: {
+          type: Schema.Types.ObjectId,
+          ref: "Users",
+        },
+      },
+    },
+    announcement: {
+      type: [Schema.Types.ObjectId],
+      ref: "Announcements",
+    },
+    students: {
+      type: [Schema.Types.ObjectId],
+      ref: "Users",
+    },
+  },
+  { timestamps: true }
+);
+
+const Boards = models.Boards || model("Boards", boardsSchema);
+export default Boards;
